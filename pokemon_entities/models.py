44a2a276816d6643aceaff587a9f59dfe7c7cf2b
leeny_the_bear <leeny@inbox.ru>
2022-08-19 17:47:04 +0700

    rename field

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,9 +2,9 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    text = models.TextField()
+    title = models.TextField()
 
     def __str__(self):
         if self:
-            return self.text
-        return f'{self.text}'
+            return self.title
+        return f'{self.title}'
