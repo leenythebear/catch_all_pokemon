97f4c8c711e61eb4c85a2cc9ee0d85a48850cd1f
leeny_the_bear <leeny@inbox.ru>
2022-08-19 17:06:47 +0700

    show Pokemon name in admin UI

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,3 +3,8 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     text = models.TextField()
+
+    def __str__(self):
+        if self:
+            return self.text
+        return f'{self.text}'
