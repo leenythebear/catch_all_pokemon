09761f7793dcb5bd8e9199ace58752af3f3e73a9
leeny_the_bear <leeny@inbox.ru>
2022-08-26 14:32:03 +0700

    show pokemons on mainpage

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -40,17 +40,25 @@ def show_all_pokemons(request):
             )
 
     pokemons_on_page = []
-    for pokemon in pokemons:
-        pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
-        })
+    for pokemon in pokemons_above_the_map:
+        if pokemon.photo:
+            img_url = request.build_absolute_uri(pokemon.photo.url)
+            pokemons_on_page.append(
+                {
+                    "pokemon_id": pokemon.id,
+                    "img_url": img_url,
+                    "title_ru": pokemon.title,
+                }
+            )
 
-    return render(request, 'mainpage.html', context={
-        'map': folium_map._repr_html_(),
-        'pokemons': pokemons_on_page,
-    })
+    return render(
+        request,
+        "mainpage.html",
+        context={
+            "map": folium_map._repr_html_(),
+            "pokemons": pokemons_on_page,
+        },
+    )
 
 
 def show_pokemon(request, pokemon_id):
