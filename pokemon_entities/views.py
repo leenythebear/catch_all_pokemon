cd986e63a4165ee862feaeac8a96d80a184a1251
leeny_the_bear <leeny@inbox.ru>
2022-08-26 14:33:44 +0700

    show pokemons on mainpage map

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -27,18 +27,20 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
-
+    pokemons_entities = PokemonEntity.objects.filter(
+        appeared_at__lt=timezone.localtime(),
+        disappeared_at__gt=timezone.localtime(),
+    )
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon in pokemons:
-        for pokemon_entity in pokemon['entities']:
-            add_pokemon(
-                folium_map, pokemon_entity['lat'],
-                pokemon_entity['lon'],
-                pokemon['img_url']
-            )
-
+    for pokemon_entity in pokemons_entities:
+        img_url = request.build_absolute_uri(pokemon_entity.pokemon.photo.url)
+        add_pokemon(
+            folium_map,
+            pokemon_entity.lat,
+            pokemon_entity.lon,
+            img_url,
+        )
+    pokemons_above_the_map = Pokemon.objects.all()
     pokemons_on_page = []
     for pokemon in pokemons_above_the_map:
         if pokemon.photo:
