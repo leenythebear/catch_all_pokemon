1c53372c392c647a04516027cc63ba6804c3f2fe
leeny_the_bear <leeny@inbox.ru>
2022-08-26 14:36:52 +0700

    format code

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -11,9 +11,9 @@ from .models import Pokemon, PokemonEntity
 
 MOSCOW_CENTER = [55.751244, 37.618423]
 DEFAULT_IMAGE_URL = (
-    'https://vignette.wikia.nocookie.net/pokemon/images/6/6e/%21.png/revision'
-    '/latest/fixed-aspect-ratio-down/width/240/height/240?cb=20130525215832'
-    '&fill=transparent'
+    "https://vignette.wikia.nocookie.net/pokemon/images/6/6e/%21.png/revision"
+    "/latest/fixed-aspect-ratio-down/width/240/height/240?cb=20130525215832"
+    "&fill=transparent"
 )
 
 
